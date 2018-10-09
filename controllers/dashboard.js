94160c4a52c3e28a7d58aa6bf5a6d21c6ec740ea
Medi Assumani <mediassumani49@gmail.com>
2018-10-09 16:49:03 -0700

    Add route to dashboard

---
+++ controllers/dashboard.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+// PATH TO MODEL
+const Charity = require("../model/charity");
+
+
+// ROUTE : CHARITIES DASHBOARD
+router.get("/dashboard", (request, response) => {
+  Charity.find()
+    .then(charities => {
+      response.render('dashboard', { charities: charities});
+    }).catch(error => {
+      console.log(error.message);
+    });
+});
+
+
+module.exports = router;
