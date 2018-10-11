05e8465bcc1accaeecf2d513224433a08b3bbc61
Medi Assumani <mediassumani49@gmail.com>
2018-10-10 18:49:03 -0700

    Use NewsApi wrapper

--- controllers/home.js
+++ controllers/home.js
@@ -12,25 +12,13 @@ const newsapi = new NewsAPI(apiKey);
 // ROUTE : HOME PAGE
 router.get('/', function(req,res){
 
-  var term = encodeURIComponent(keyword);
-  var url = "https://newsapi.org/v2/everything?q=" + term + "&apiKey=" + apiKey;
-
-  https.get(url, function(response){
-
-    response.setEncoding('utf8');
-    var dataToBeReturned = "";
-
-    response.on("data", function(inComingData){
-      // Continuously update stream with data
-      dataToBeReturned += inComingData;
-    });
-    response.on("end", function(){
-      var parsedDataFromApi = JSON.parse(dataToBeReturned);
-      console.log(parsedDataFromApi.articles[0]);
-      res.render('home', { news: parsedDataFromApi.data});
-    });
+  newsapi.v2.everything({
+    q: keyword,
+    language: 'en',
+    sortBy: 'relevancy'
+  }).then(response => {
+    console.log(response.articles[0].source);
   });
-
 });
 
 
